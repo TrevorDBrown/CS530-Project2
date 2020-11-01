4bd60a38a81cd3d141fbc22963ba7f31e6682e36
Trevor Brown <trevordylanbrown@gmail.com>
2020-11-01 16:01:11 -0600

    Create Test.java
    Test Java source code file.

---
+++ Test.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.print("Hello world!");
+        int test = 1;
+        double test2 = 2.0;
+        int test3 = 4 * 3;
+        String test4 = "This is a test string!";
+        boolean testBoolean = true;
+
+        /*
+            This is a test comment in a file.
+        */
+
+        test = 3;
+        test2 -= 2.0;
+        test3 *= 3;
+        double test5 = 5;
+        test5 /= 1;
+        double test6 = 20;
+        test6 %= 5;
+        test4 = "This is an updated test string!";
+
+        if (testBoolean){
+            System.out.print("Testing!")
+        }
+    }
+}
